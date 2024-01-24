b6a613535a06919adb6031d2b44c5ff2ef645138
sertemo <tejedor.moreno@gmail.com>
2024-01-24 16:29:16 +0100

    Terminado capitulo 1

--- clt_app/clt_demo.py
+++ clt_app/clt_demo.py
@@ -30,4 +30,30 @@ fig2, ax2 = plt.subplots()
 ax2 = plt.hist([1, 1, 1, 1])
 st.pyplot(fig2)
 
+st.title('Illustrating the Central Limit Theorem with Streamlit') 
+st.header('An App by STM 2024 (c)') 
+st.write(('''This app simulates a thousand coin flips using the chance of heads input below,  
+and then samples with replacement from that population and plots the histogram of the
+means of the samples, in order to illustrate the Central Limit Theorem!''')) 
+
+
+perc_heads = st.number_input(label='Chance of Coins Landing on Heads', min_value=0.0, max_value=1.0, value=.5) 
+graph_title = st.text_input('Graph Title')
+
+binom_dist = np.random.binomial(1, perc_heads, 1000) 
+
+list_of_means = []
+for i in range(0, 1000): 
+    list_of_means.append(np.random.choice(binom_dist, 100, replace=True).mean()) 
+
+fig, ax = plt.subplots() 
+ax = plt.hist(list_of_means, range=[0, 1]) 
+plt.title(graph_title)
+
+st.pyplot(fig)
+
+st.markdown("""
+            # Hola
+            ## Hola
+            ### Hola""")
 
